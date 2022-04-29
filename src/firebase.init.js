b2b788b0fb9e480b2a8010b37320e984feb69dc7
masud <abdullahalmasud01737906772@gmail.com>
2022-04-29 15:36:04 +0600

    hosting firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,13 +6,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRJ0wOqvtB3dDt0w1D36AYIZo-kRgPQe8",
-  authDomain: "computer-warehouse-5a411.firebaseapp.com",
-  projectId: "computer-warehouse-5a411",
-  storageBucket: "computer-warehouse-5a411.appspot.com",
-  messagingSenderId: "949583365887",
-  appId: "1:949583365887:web:b47b79cdc6d6415cbb2eca"
-};
+    apiKey: "AIzaSyBRJ0wOqvtB3dDt0w1D36AYIZo-kRgPQe8",
+    authDomain: "computer-warehouse-5a411.firebaseapp.com",
+    projectId: "computer-warehouse-5a411",
+    storageBucket: "computer-warehouse-5a411.appspot.com",
+    messagingSenderId: "949583365887",
+    appId: "1:949583365887:web:b47b79cdc6d6415cbb2eca"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
