df4ba0e02476fadaf5d7e4c8ea6c2b8900f9b87a
masud <abdullahalmasud01737906772@gmail.com>
2022-05-03 03:10:40 +0600

    use axios and jwt

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-
 import Loading from '../../Shared/Loading/Loading';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 const Login = () => {
 
@@ -33,7 +34,7 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+       // navigate(from, { replace: true });
     }
 
     if (error) {
@@ -42,13 +43,15 @@ const Login = () => {
 
     }
 
-
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwrodRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const {data} = await axios.post('http://localhost:5000/login', {email});
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
 
     const resetPassword = async () => {
